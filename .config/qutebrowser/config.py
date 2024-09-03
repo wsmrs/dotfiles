7cf76a3d13e07f755ed0c3567ba904118a6d66a3
Will Sommers <contact@willsommers.com>
2024-09-03 07:44:32 -0400

    qutebrowser: save session

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,3 +2,4 @@ config.load_autoconfig()
 
 c.downloads.location.directory = '$HOME'
 c.editor.command = ['footclient', 'vim', '-f', '{file}', '-c', 'normal {line}G{column0}l']
+c.auto_save.session = True
