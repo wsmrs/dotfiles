e7b8122a84534b3f4b19040cbb67db151f7ed76e
Will Sommers <contact@willsommers.com>
2024-09-16 00:18:18 -0400

    Revert "qutebrowser: save session"
    This reverts commit 7cf76a3d13e07f755ed0c3567ba904118a6d66a3.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,4 +2,3 @@ config.load_autoconfig()
 
 c.downloads.location.directory = '$HOME'
 c.editor.command = ['footclient', 'vim', '-f', '{file}', '-c', 'normal {line}G{column0}l']
-c.auto_save.session = True
